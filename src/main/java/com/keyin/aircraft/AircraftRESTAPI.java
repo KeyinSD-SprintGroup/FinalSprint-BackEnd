76e58a63b1ab37266fcd7a22f467e2952e3bb947
KimS112022 <Kimberley.Snow@Keyin.com>
2023-12-11 13:33:32 -0330

    set up Entities with Attributes, set up API for each Enitity

--- src/main/java/com/keyin/aircraft/AircraftRESTAPI.java
+++ src/main/java/com/keyin/aircraft/AircraftRESTAPI.java
@@ -1,4 +1,12 @@
 package com.keyin.aircraft;
 
-public interface AircraftRESTAPI {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@RepositoryRestResource(collectionResourceRel = "aircraft", path = "aircraft")
+public interface AircraftRESTAPI extends JpaRepository<Aircraft, Long> {
+    public List<Aircraft> findByTailNumber(@RequestParam String tailNumber);
 }
--- src/main/java/com/keyin/city/CityRESTAPI.java
+++ src/main/java/com/keyin/city/CityRESTAPI.java
@@ -1,4 +1,12 @@
 package com.keyin.city;
 
-public interface CityRESTAPI {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@RepositoryRestResource(collectionResourceRel = "city", path = "city")
+public interface CityRESTAPI extends JpaRepository<City, Long> {
+    public List<City> findByNameAndState(@RequestParam String name, String state);
 }
