9d71cb89c13a03b50fe0c9737658ebb6e942bced
Glen Sturge <117656215+glen-sturge@users.noreply.github.com>
2023-12-17 22:00:20 -0330

    updated various relationships and CORS config

---
+++ src/main/java/com/keyin/CorsConfig.java
@@ -0,0 +1,29 @@
+package com.keyin;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+
+        // Allow requests from your React app's origin (http://localhost:3000)
+        config.addAllowedOrigin("https://your-react-app-domain.com");
+
+        // Allow all HTTP methods (GET, POST, PUT, DELETE, etc.).
+        config.addAllowedMethod("*");
+
+        // Allow all headers.
+        config.addAllowedHeader("*");
+
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
